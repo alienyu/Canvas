a5486b46cbfc3d8a11f36c732a606a3623213b88
Alien Yu <alienyu@Aliens-MacBook-Pro.local>
2016-04-26 19:17:52 +0800

    Dg

--- drawImg/drawImg.js
+++ drawImg/drawImg.js
@@ -3,43 +3,74 @@
  */
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = "000"; //画布背景色
+ctx.fillStyle = "#000"; //画布背景色
 var drawObject = {
+    canvasPos: {
+        left: 0,
+        right: $("canvas").width(),
+        top: 0,
+        bottom: $("canvas").height(),
+        offsetTop: $("canvas").offset().top,
+        offsetLeft: $("canvas").offset().left,
+        width: $("canvas").width(),
+        height: $("canvas").height()
+    }, //canvas位置大小
     imgOriginPos: {}, //图像原始位置,尺寸
     imgResizePos: {}, //图像变化后的位置和尺寸
     rectPos: {}, //裁剪矩形框位置和尺寸
-    resCorner: {} //改变尺寸的四个小框的位置
+    resRectPos: {}, //裁剪框变化后的位置和尺寸
+    resCorner: {}, //改变尺寸的四个小框的位置
+    ratio: 1, //比例
+    isMouseDown: false, //鼠标是否按下
+    hitType: "", //点击目标是右下角还是拖拽框
+    mousePos: {} //鼠标偏移量
 }
-
-function clip(path, type, ratio) {
-    ctx.clearRect(0, 0, 800, 500);
-    drawImg(path, type, ratio);
-}
-
-function drawImg(path, type, ratio) {
-    if(type == "new") {
-        canvasImg = new Image();
-        canvasImg.src = path;
-        canvasImg.onload = function() {
-            var position = drawObject.imgOriginPos = calculateImg(this, canvas);
-            ctx.drawImage(canvasImg, position.left, position.top, position.width, position.height)
-            drawRect();
-            drawResCorner(20);
-        }
-    } else {
-        drawObject.imgResizePos = drawObject.imgOriginPos;
-        var resizeResult = drawObject.imgResizePos = resizeImg(ratio);
-        ctx.drawImage(canvasImg, resizeResult.left, resizeResult.top, resizeResult.width, resizeResult.height);
-        drawRect();
-        drawResCorner(20);
+function init(path) {
+    canvasImg = new Image();
+    canvasImg.src = path;
+    canvasImg.onload = function() {
+        calculateImg(this, canvas);
+        cpuRectPos();
+        cpuCorner("init", 20);
+        draw("init");
     }
+}
+
+function draw(type) {
+    ctx.clearRect(0, 0, 800, 500);
+    drawImg();
+    drawRect(type);
+    drawResCorner(20);
+}
+
+function drawImg() {
+    var pos = drawObject.imgResizePos;
+    ctx.drawImage(canvasImg, pos.left, pos.top, pos.width, pos.height);
+}
+
+function drawRect(type) {
+    ctx.strokeStyle = 'red';
+    var pos = {};
+    if(type == "init") {
+        pos = drawObject.rectPos;
+    } else {
+        pos = drawObject.resRectPos;
+    }
+    ctx.strokeRect(pos.left, pos.top, pos.width, pos.height);
+}
+
+function drawResCorner(size) {
+    var pos = drawObject.resCorner;
+    var color = "red";
+    ctx.fillStyle = color;
+    ctx.fillRect(pos.left, pos.top, size, size);
 }
 
 function calculateImg(img, canvas) {
     var imgWidth = img.width,
         imgHeight = img.height,
-        canvasWidth = canvas.width,
-        canvasHeight = canvas.height,
+        canvasWidth = drawObject.canvasPos.width,
+        canvasHeight = drawObject.canvasPos.height,
         top = 0,
         left = 0,
         target = "width";
@@ -53,7 +84,7 @@ function calculateImg(img, canvas) {
         left = parseInt((canvasWidth - imgWidth) / 2,10);
         target = "height";
     }
-    return {
+    drawObject.imgResizePos = drawObject.imgOriginPos = {
         target: target,
         left: left,
         top: top,
@@ -62,7 +93,8 @@ function calculateImg(img, canvas) {
     }
 }
 
-function resizeImg(ratio) {
+function resizeImg() {
+    var ratio = drawObject.ratio;
     var canvasImgPos = drawObject.imgOriginPos;
     var startWidth = canvasImgPos.width,
         startHeight = canvasImgPos.height,
@@ -72,18 +104,12 @@ function resizeImg(ratio) {
         currentHeight = parseInt(startHeight*ratio ,10);
     var currentTop = startPosTop - parseInt((currentHeight - startHeight) / 2, 10),
         currentLeft = startPosLeft - parseInt((currentWidth - startWidth) / 2, 10)
-    return {
+    drawObject.imgResizePos = {
         left: currentLeft,
         top: currentTop,
         width:currentWidth,
         height: currentHeight
     }
-}
-
-function drawRect() {
-    ctx.strokeStyle = 'red';
-    var cutRectPos = drawObject.rectPos = cpuRectPos();
-    ctx.strokeRect(cutRectPos.left, cutRectPos.top, cutRectPos.width, cutRectPos.height);
 }
 
 function cpuRectPos() {
@@ -97,73 +123,175 @@ function cpuRectPos() {
     }
     var top = parseInt((canvas.height - height) / 2, 10);
     var left = parseInt((canvas.width - width) / 2, 10);
-    return {
+    drawObject.resRectPos = drawObject.rectPos = {
         width: width,
         height: height,
         top: top,
         left: left,
+        bottom: top + height,
+        right: left + width,
         corner: {
-            first: {
-                top: top,
-                left: left
-            },
-            second: {
-                top: top,
-                left: left + width
-            },
-            third: {
-                top: top + height,
-                left: left + width
-            },
-            forth: {
-                top: top + height,
-                left: left
+            top: top + height,
+            left: left + width
+        }
+    }
+}
+
+function cpuCorner(type, size) {
+    var point = {};
+    if(type == "init") {
+        point = drawObject.rectPos.corner;
+    } else {
+        point = drawObject.resRectPos.corner;
+    }
+    drawObject.resCorner = {
+        top: point.top - size,
+        bottom: point.top,
+        left: point.left - size,
+        right: point.left
+    };
+}
+
+function resRectPos(type, mousePos) {
+    var edge = {
+        left: drawObject.imgResizePos.left > 0 ? drawObject.imgResizePos.left : drawObject.canvasPos.left,
+        right: drawObject.imgResizePos.left > 0 ? drawObject.imgResizePos.left + drawObject.imgResizePos.width : drawObject.canvasPos.right,
+        top: drawObject.imgResizePos.top > 0 ? drawObject.imgResizePos.top : drawObject.canvasPos.top,
+        bottom: drawObject.imgResizePos.top > 0 ? drawObject.imgResizePos.top + drawObject.imgResizePos.height : drawObject.canvasPos.bottom
+    };
+    var rectLeft = drawObject.rectPos.left,
+        rectTop = drawObject.rectPos.top,
+        rectWidth = drawObject.rectPos.width,
+        rectHeight = drawObject.rectPos.height;
+    if(type == "corner") {
+        var newHeight = rectHeight + mousePos.y;
+        var newWidth = parseInt(newHeight * 1.6, 10);
+        //宽度超出右边界
+        if(rectLeft + newWidth > edge.right) {
+            newWidth = edge.right - rectLeft;
+            newHeight = parseInt(newWidth / 1.6, 10);
+        }
+        //高度超出下边界
+        if(rectTop + newHeight > edge.bottom) {
+            newHeight = edge.bottom - rectTop;
+            newWidth = parseInt(newHeight * 1.6, 10);
+        }
+        if(newHeight < 40) {
+            newHeight = 40;
+            newWidth = 64;
+        }
+        drawObject.resRectPos = {
+            width: newWidth,
+            height: newHeight,
+            top: rectTop,
+            left: rectLeft,
+            bottom: rectTop + newHeight,
+            right: rectLeft + newWidth,
+            corner: {
+                top: rectTop + newHeight,
+                left: rectLeft + newWidth
+            }
+        }
+    } else {
+        var newLeft = rectLeft + mousePos.x,
+            newTop = rectTop + mousePos.y,
+            newRight = newLeft + rectWidth,
+            newBottom = newTop + rectHeight;
+        if(newLeft < edge.left) {
+            newLeft = edge.left;
+        }
+        if(newRight > edge.right) {
+            newLeft = edge.right - rectWidth;
+        }
+        if(newTop < edge.top) {
+            newTop = edge.top;
+        }
+        if(newBottom > edge.bottom) {
+            newTop = edge.bottom - rectHeight;
+        }
+        drawObject.resRectPos = {
+            width: rectWidth,
+            height: rectHeight,
+            top: newTop,
+            left: newLeft,
+            bottom: newTop + rectHeight,
+            right: newLeft + rectWidth,
+            corner: {
+                top: newTop + rectHeight,
+                left: newLeft + rectWidth
             }
         }
     }
-
 }
-function drawResCorner(size) {
-    var cornerPos = drawObject.resCorner = cpuCorner(size);
-    var color = "red";
-    for(var key in cornerPos) {
-        var data = cornerPos[key];
-        ctx.fillStyle = color;
-        ctx.fillRect(data.left, data.top, size, size);
+
+function ratioRecPos() {
+    var edge = {
+        left: drawObject.imgResizePos.left > 0 ? drawObject.imgResizePos.left : drawObject.canvasPos.left,
+        right: drawObject.imgResizePos.left > 0 ? drawObject.imgResizePos.left + drawObject.imgResizePos.width : drawObject.canvasPos.right,
+        top: drawObject.imgResizePos.top > 0 ? drawObject.imgResizePos.top : drawObject.canvasPos.top,
+        bottom: drawObject.imgResizePos.top > 0 ? drawObject.imgResizePos.top + drawObject.imgResizePos.height : drawObject.canvasPos.bottom
+    };
+    var rect = {
+        left: drawObject.rectPos.left,
+        right: drawObject.rectPos.right,
+        top: drawObject.rectPos.top,
+        bottom: drawObject.rectPos.bottom
+    };
+    var delta = 0,
+        newWidth = drawObject.rectPos.width,
+        newHeight = drawObject.rectPos.height;
+    if(rect.top < edge.top) {
+        delta = edge.top - rect.top;
+        rect.top = edge.top;
+        newHeight = drawObject.imgResizePos.height > drawObject.rectPos.height ? drawObject.rectPos.height : (drawObject.rectPos.height - delta);
+        newWidth = parseInt(newHeight * 1.6, 10);
+    }
+    if(rect.bottom > edge.bottom) {
+        delta = rect.bottom - edge.bottom;
+        newHeight = drawObject.imgResizePos.height > drawObject.rectPos.height ? drawObject.rectPos.height : (drawObject.rectPos.height - delta);
+        newWidth = parseInt(newHeight * 1.6, 10);
+        rect.top = edge.bottom - newHeight;
+    }
+    if(rect.left < edge.left) {
+        delta = edge.left - rect.left;
+        newWidth = drawObject.imgResizePos.width > drawObject.rectPos.width ? drawObject.rectPos.width : (drawObject.rectPos.width - delta);
+        newHeight = parseInt(newWidth / 1.6, 10);
+        rect.left = edge.left;
+    }
+    if(rect.right > edge.right) {
+        delta = rect.right - edge.right;
+        newWidth = drawObject.imgResizePos.width > drawObject.rectPos.width ? drawObject.rectPos.width : (drawObject.rectPos.width - delta);
+        newHeight = parseInt(newWidth / 1.6, 10);
+        rect.left = edge.right - newWidth;
+    }
+    drawObject.rectPos = {
+        width: newWidth,
+        height: newHeight,
+        top: rect.top,
+        left: rect.left,
+        bottom: rect.top + newHeight,
+        right: rect.left + newWidth,
+        corner: {
+            top: rect.top + newHeight,
+            left: rect.left + newWidth
+        }
     }
 }
 
-function cpuCorner(size) {
-    var first = drawObject.rectPos.corner.first,
-        second = drawObject.rectPos.corner.second,
-        third = drawObject.rectPos.corner.third,
-        forth = drawObject.rectPos.corner.forth;
-    var cornerPos = {};
-    cornerPos.first = {
-        top: first.top,
-        bottom: first.top + size,
-        left: first.left,
-        right: first.left + size
-    };
-    cornerPos.second = {
-        top: second.top,
-        bottom: second.top + size,
-        left: second.left - size,
-        right: second.left
-    };
-    cornerPos.third = {
-        top: third.top - size,
-        bottom: third.top,
-        left: third.left - size,
-        right: third.left
-    };
-    cornerPos.forth = {
-        top: forth.top - size,
-        bottom: forth.top,
-        left: forth.left,
-        right: forth.left + size
-    };
-    return cornerPos;
+//反推裁剪框的实际位置
+function refreshRectPos() {
+    drawObject.rectPos = drawObject.resRectPos = {
+        left: drawObject.rectPos.left,
+        top: drawObject.rectPos.top,
+        right: drawObject.resCorner.right,
+        bottom: drawObject.resCorner.bottom,
+        width: drawObject.resCorner.right - drawObject.rectPos.left,
+        height: drawObject.resCorner.bottom - drawObject.rectPos.top,
+        corner: {
+            top: drawObject.resCorner.top,
+            left: drawObject.resCorner.left
+        }
+    }
 }
 
 function isHit(pos,mouse) {
@@ -173,10 +301,8 @@ function isHit(pos,mouse) {
         bottom = pos.bottom,
         mouseX = mouse.x,
         mouseY = mouse.y;
-    var canvasPosLeft = $("canvas").offset().left,
-        canvasPosTop = $("canvas").offset().top;
-    var relativeLeft = mouseX - canvasPosLeft;
-    var relativeTop = mouseY - canvasPosTop;
+    var relativeLeft = mouseX - drawObject.canvasPos.offsetLeft;
+    var relativeTop = mouseY - drawObject.canvasPos.offsetTop;
     if(relativeLeft > left && relativeLeft < right && relativeTop > top && relativeTop < bottom) {
         return true;
     } else {
@@ -188,12 +314,44 @@ $("#updImg").on("change" ,function() {
     var file = new FileReader;
     file.readAsDataURL(this.files[0]);
     file.onloadend = function() {
-        clip(this.result,"new", 1);
+        init(this.result);
     }
 })
 $("#slider").slider();
 $("#slider").on("slide", function(slideEvt) {
-    clip("","resize", (1+slideEvt.value/100));
+    drawObject.ratio = (1+slideEvt.value/100);
+    resizeImg();
+    ratioRecPos();
+    cpuCorner('init', 20);
+    draw("init");
+    refreshRectPos();
 });
 
+$("body").bind({
+    "mousedown": function(e) {
+        drawObject.isMouseDown = true;
+        drawObject.mousePos = {
+            startX: e.pageX,
+            startY: e.pageY
+        }
+        if(isHit(drawObject.resCorner, {x: e.pageX, y: e.pageY})) {
+            drawObject.hitType = "corner";
+        } else if(isHit(drawObject.rectPos, {x: e.pageX, y: e.pageY})){
+            drawObject.hitType = "rect";
+        }
+    },
+    "mousemove": function(e) {
+        if(drawObject.isMouseDown && drawObject.hitType) {
+            var x = e.pageX - drawObject.mousePos.startX,
+                y = e.pageY - drawObject.mousePos.startY;
+            resRectPos(drawObject.hitType, {x: x, y: y});
+            cpuCorner("", 20);
+            draw();
+        }
+    },
+    "mouseup": function() {
+        drawObject.isMouseDown = false;
+        drawObject.rectPos = drawObject.resRectPos;
+    }
+});
 
